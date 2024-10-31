5470edc5e81b1e96ba2f9e0400a6f4713d62f9e3
wxr <darkfor9es0t@163.com>
2024-10-31 20:29:04 +0800

    5.1.1 3.菜单栏 QActionGroup类

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QActionGroup>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -12,6 +14,21 @@ MainWindow::MainWindow(QWidget *parent)
         QIcon(":/image/myimages/open.png"), tr("打开文件(&O)"));
     action_Open->setShortcut(QKeySequence("Ctrl+O"));   // 添加快捷键
     ui->menubar->addAction(action_Open);
+
+
+    QActionGroup *group = new QActionGroup(this);
+    QAction *action_L = group->addAction(tr("左对齐(&L)"));
+    action_L->setCheckable(true);
+    QAction *action_R = group->addAction(tr("右对齐(&R)"));
+    action_R->setCheckable(true);
+    QAction *action_C = group->addAction(tr("居中(&C)"));
+    action_C->setCheckable(true);
+    action_L->setChecked(true);
+    editMenu->addSeparator();
+    editMenu->addAction(action_L);
+    editMenu->addAction(action_R);
+    editMenu->addAction(action_C);
+
 }
 
 MainWindow::~MainWindow()
