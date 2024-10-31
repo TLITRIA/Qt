4e1a5fb9a02670726ff836d05a88166917a94de1
wxr <darkfor9es0t@163.com>
2024-10-31 20:22:09 +0800

    5.1.1 2.编写代码的方式添加菜单

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,12 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QMenu *editMenu = ui->menubar->addMenu(tr("编辑(&E)"));   // 添加编辑菜单
+    QAction *action_Open = editMenu->addAction(         // 添加打开菜单
+        QIcon(":/image/myimages/open.png"), tr("打开文件(&O)"));
+    action_Open->setShortcut(QKeySequence("Ctrl+O"));   // 添加快捷键
+    ui->menubar->addAction(action_Open);
 }
 
 MainWindow::~MainWindow()
