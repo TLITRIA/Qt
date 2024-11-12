3801e09c1eca71c022ff5211202403e4817de9e5
wxr <darkfor9es0t@163.com>
2024-11-12 12:18:15 +0800

    10.1 基本绘制和填充
    - 3.使用画刷

--- QtCreatorQuickStart/mydrawing.cpp
+++ QtCreatorQuickStart/mydrawing.cpp
@@ -23,9 +23,30 @@ void MyDrawing::paintEvent(QPaintEvent *event)
     // painter.end();
 
     QPen pen(Qt::green, 5, Qt::DotLine, Qt::RoundCap, Qt::RoundJoin);
+    /*        画刷    线宽  画笔风格    画笔端点风格  画笔连接风格    */
     painter.setPen(pen);
     QRectF rectangle(70.0, 40.0, 80.0, 60.0);
     int startAngle = 30*16;
     int spanAngle = 120*16;
     painter.drawArc(rectangle, startAngle, spanAngle); // 绘制圆弧
+
+    pen.setWidth(1);
+    pen.setStyle(Qt::SolidLine);
+    painter.setPen(pen);
+    painter.drawRect(160, 20, 50, 40);
+    QBrush brush(QColor(0,0,225), Qt::Dense4Pattern);
+    painter.setBrush(brush);
+    painter.drawEllipse(220,20,50,50);
+    brush.setTexture(QPixmap("D:/Documents/QtCreatorQuick/QtCreatorQuickStart/mydrawing/yafeilinux.png"));
+    painter.setBrush(brush);
+    static const QPointF points[4] = {
+        QPointF(270.0, 80.0),
+        QPointF(290.0, 10.0),
+        QPointF(350.0, 30.0),
+        QPointF(390.0, 70.0)
+    };
+    painter.drawPolygon(points, 4);  // 使用点的数组绘制多边形
+
+    painter.fillRect(QRect(10, 100, 150, 20), QBrush(Qt::darkYellow));
+    painter.eraseRect(QRect(50, 0, 50, 120));
 }
